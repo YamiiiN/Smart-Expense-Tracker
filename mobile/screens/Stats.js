7fb7c8f67411d0aed4f3c5f88d365fddbd0bc3ae
ikyyyyl <fabromikylla@gmail.com>
2025-11-02 23:25:36 +0800

    fixed

--- mobile/screens/Stats.js
+++ mobile/screens/Stats.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { View, Text } from 'react-native'
+import styles from "../styles/commonStyles";
 
 export default function Stats() {
   return (
-    <View>
-      <Text>Stats Screen</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Stats Screen</Text>
     </View>
   )
 }
